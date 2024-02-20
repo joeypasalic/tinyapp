5c5c7116ade6cfd8f699f471af45f58b36b56ed8
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-19 20:51:35 -0500

    add stores new url in urlDatabase upon creating new url, as well as redirecting to /id page

--- express_server.js
+++ express_server.js
@@ -3,10 +3,11 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const generateRandomString = function() {
-//function picks a random character from chars string 6 times for our random string
+  //function picks a random character from chars string 6 times for our random string
   const length = 6;
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let randomString = '';
+
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * chars.length);
     randomString += chars[randomIndex];
@@ -22,8 +23,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-//convert the request body from a Buffer into readable string
-app.use(express.urlencoded({ extended: true}));
+
+//convert the request body from a Buffer into a readable string
+app.use(express.urlencoded({ extended: true }));
 
 //when url ends with just / (home page) it will say hello
 app.get("/", (req, res) => {
@@ -42,7 +44,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -53,21 +54,33 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); //log the POST request body to console
-  res.send(generateRandomString()); //outputs as a way for me to test it for now
+
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+
 });
 
 //handler to link the short and long urls WITHOUT redirecting instantly
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
+
   if (longURL) {
     const templateVars = { id, longURL };
     res.render("urls_show", templateVars);
-  }
-  
-  /*else {
+  } else {
     res.status(404).send("Error - Short URL not found!");
-  }*/
+  }
+
 });
 
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  
+  if (longURL) {
+    res.redirect(longURL);
+  }
+});

8ff929c8c1dd37477d85a30ee67f28d3addfcb69
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-21 20:02:45 -0500

    add GET route to display username

--- express_server.js
+++ express_server.js
@@ -47,11 +47,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -84,7 +79,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 //on login
 app.post("/login", (req, res) => {
-  const username = req.body;
+  const username = req.body.username;
   res.cookie('username', username);
   res.redirect('/urls');
   
@@ -112,5 +107,14 @@ app.get("/u/:id", (req, res) => {
   if (longURL) {
     res.redirect(longURL);
   }
+});
+
+app.get("/urls", (req, res) => {
+
+  const templateVars = {
+    username: req.cookies.username,
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
 });
 
